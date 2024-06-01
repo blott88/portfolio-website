eaddfe34418c7dc05d951f8adf578be36a7e220f
blott88 <160795807+blott88@users.noreply.github.com>
2024-06-01 11:25:54 -0400

    telling vite.config where i put index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: './', // Ensure this points to the root directory where the `index.html` is located
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './public/index.html', // Ensure this path is correct
+    },
+  },
+});
